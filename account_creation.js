82061495b42ea14bfb510ec619d9d8d22f1ed286
Cristi Virlan <rethvol111@gmail.com>
2023-07-17 15:39:43 +0300

    Added confirm password option

--- account_creation.js
+++ account_creation.js
@@ -4,25 +4,30 @@ button.addEventListener('click', function() {
     const username = document.querySelector("#first input").value;
     const email = document.querySelector("#second input").value;
     const password = document.querySelector("#third input").value;
+    const cPassword = document.querySelector("#fourth input").value;
 
-    var object = {
-        "username": username,
-        "email": email,
-        "password": password
-    };
-
-    fetch("", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(object)
-    }).then((response) => response.json())
-    .then((data) => {
-        alert(data.message);
-        
-        if(data.id == 1) {
-            window.location.href = "login.html";
-        }
-    })
+    if(password == cPassword) {
+        var object = {
+            "username": username,
+            "email": email,
+            "password": password
+        };
+    
+        fetch("", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(object)
+        }).then((response) => response.json())
+        .then((data) => {
+            alert(data.message);
+            
+            if(data.id == 1) {
+                window.location.href = "login.html";
+            }
+        });
+    } else {
+        alert("Passwords don't match!");
+    }
 });
