ac9013ffcf3537935d9a5e349c3c5d10e7febd2d
virlancristian <rethvol111@gmail.com>
2023-08-10 02:12:57 +0300

    Initial upload

---
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -0,0 +1,23 @@
+package com.example.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/").allowedOrigins("http://82.76.142.241:5500");
+			}
+		};
+	}
+}
---
+++ index.js
@@ -0,0 +1,11 @@
+function goToAccCreation() {
+    window.location.href = "account_creation.html";
+}
+
+function goToLogin() {
+    window.location.href = "login.html";
+}
+
+function goBack() {
+    window.location.href = "index.html";
+}
